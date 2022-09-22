6d7244bda4dbddef5e404b026e745693b0f85621
Isaac Naylor <Isaac.Naylor@Jet2.com>
2022-09-22 09:54:31 +0100

    updated some tests

--- tests/test_data.py
+++ tests/test_data.py
@@ -70,3 +70,27 @@ def test_get_valid_holidays_category_either_returns_all_categories() -> None:
     ]
     assert len(get_valid_holidays(_holidays, "Category", "either")) == 3
     assert get_valid_holidays(_holidays, "Category", "either") == _holidays
+
+
+def test_get_valid_holidays_match_setting_is_valid_holiday() -> None:
+    _holidays = [
+        Holiday(
+            1,
+            "HotelName",
+            "Leeds",
+            "Europe",
+            "England",
+            "active",
+            1,
+            "cold",
+            "city",
+            80
+        )
+    ]
+
+    assert len(get_valid_holidays(_holidays, "Continent", "asia")) == 0
+    assert len(get_valid_holidays(_holidays, "TempRating", "cold")) == 1
+    assert len(get_valid_holidays(_holidays, "Location", "city")) == 1
+    assert len(get_valid_holidays(_holidays, "PricePerNight", 85)) == 1
+    assert len(get_valid_holidays(_holidays, "PricePerNight", 80)) == 1
+    assert len(get_valid_holidays(_holidays, "PricePerNight", 75)) == 0
